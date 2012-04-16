6517e5cdfc7520e0c970fc7207002c104fe20b8a
Ryosuke Miyahara <r_panda007@yahoo.co.jp>
2012-04-17 07:33:27 +0900

    練習問題3.10

---
+++ java_training/ch03/ex03_10/LinkedList.java
@@ -0,0 +1,115 @@
+package ex03_10;
+
+public class LinkedList implements Cloneable
+{
+
+    private Object object;
+    private LinkedList nextObject;
+
+    public LinkedList clone()
+    {
+        try
+        {
+            // デフォルトの仕組みで十分
+            return (LinkedList) super.clone();
+        } catch (CloneNotSupportedException e)
+        {
+            // 起こり得ない。このクラスとObjectは複製できる
+            throw new InternalError(e.toString());
+        }
+    }
+
+    public int getObjectNumber()
+    {
+        int objectNumber = 0;
+        LinkedList next = nextObject;
+        while (true)
+        {
+            objectNumber++;
+            if (next == null)
+            {
+                break;
+            } else
+            {
+                next = nextObject.getNextObject();
+            }
+        }
+        return objectNumber;
+    }
+
+    public Object getObject()
+    {
+        return object;
+    }
+
+    public void setObject(Object target)
+    {
+        object = target;
+    }
+
+    public LinkedList getNextObject()
+    {
+        return nextObject;
+    }
+
+    public void setNextObject(LinkedList target)
+    {
+        nextObject = target;
+    }
+
+    LinkedList(Object associatedObject)
+    {
+        object = associatedObject;
+        nextObject = null;
+    }
+
+    public String toString()
+    {
+        String desc = "This method is toString of LinkedList!";
+        return desc;
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+
+        // リストが参照しているオブジェクトに対する変更は、他方のリストから見える
+        Vehicle temporary = new Vehicle("test1");
+
+        LinkedList link1 = new LinkedList(temporary);
+
+        temporary = new Vehicle("test2");
+        link1.setNextObject(new LinkedList(temporary));
+
+        LinkedList link2 = new LinkedList(null);
+        link2 = link1.clone();
+
+        System.out.println(((Vehicle)link1.getObject()).getOwner());
+        System.out.println(((Vehicle)link2.getObject()).getOwner());
+
+        // link1からもlink2からも変更が見える
+        ((Vehicle)(link2).getObject()).setOwner("modified");
+
+        System.out.println("");
+        System.out.println(((Vehicle)link1.getObject()).getOwner());
+        System.out.println(((Vehicle)link2.getObject()).getOwner());
+
+
+        // １つのリストに対する変更は、他方のリストに影響しない（link2のリストを別の参照にする）
+        temporary = new Vehicle("test3");
+
+        link2 = new LinkedList(temporary);
+
+        temporary = new Vehicle("test4");
+        link2.setNextObject(new LinkedList(temporary));
+
+        System.out.println("");
+        System.out.println(((Vehicle)link1.getObject()).getOwner());
+        System.out.println(((Vehicle)link2.getObject()).getOwner());
+
+
+    }
+
+}
