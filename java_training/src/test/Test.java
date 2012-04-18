78b64619fa86c6c0a1b16538c1e9f534ab748a54
Ryosuke Miyahara <r_panda007@yahoo.co.jp>
2012-04-19 08:56:15 +0900

    練習問題4.3

--- java_training/src/test/Test.java
+++ java_training/src/test/Test.java
@@ -7,7 +7,9 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		Object test = "a";
 		System.out.println("!");
+		System.out.println(test.toString());
 	}
 
 }
