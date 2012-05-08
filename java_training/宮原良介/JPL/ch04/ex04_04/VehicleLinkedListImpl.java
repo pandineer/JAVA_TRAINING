e3ba50ce3ab6a1ca4b5d24e6aedcca75013da291
pandineer <pandineer@gmail.com>
2012-05-08 20:12:31 +0900

    4.4更新

---
+++ java_training/宮原良介/JPL/ch04/ex04_04/VehicleLinkedListImpl.java
@@ -0,0 +1,115 @@
+package ex04_04;
+
+public class VehicleLinkedListImpl implements Cloneable, VehicleLinkedList
+{
+
+    private Vehicle vehicle;
+    private VehicleLinkedListImpl nextVehicle;
+
+    public VehicleLinkedListImpl clone()
+    {
+        try
+        {
+            // デフォルトの仕組みで十分
+            return (VehicleLinkedListImpl) super.clone();
+        } catch (CloneNotSupportedException e)
+        {
+            // 起こり得ない。このクラスとObjectは複製できる
+            throw new InternalError(e.toString());
+        }
+    }
+
+    public int getVehicleNumber()
+    {
+        int vehicleNumber = 0;
+        VehicleLinkedListImpl next = nextVehicle;
+        while (true)
+        {
+            vehicleNumber++;
+            if (next == null)
+            {
+                break;
+            } else
+            {
+                next = (VehicleLinkedListImpl)nextVehicle.getNextVehicle();
+            }
+        }
+        return vehicleNumber;
+    }
+
+    public Vehicle getVehicle()
+    {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle target)
+    {
+        vehicle = target;
+    }
+
+    public VehicleLinkedList getNextVehicle()
+    {
+        return nextVehicle;
+    }
+
+    public void setNextVehicle(VehicleLinkedListImpl target)
+    {
+        nextVehicle = target;
+    }
+
+    VehicleLinkedListImpl(Vehicle associatedVehicle)
+    {
+        vehicle = associatedVehicle;
+        nextVehicle = null;
+    }
+
+    public String toString()
+    {
+        String desc = "This method is toString of LinkedList!";
+        return desc;
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+
+        // リストが参照しているオブジェクトに対する変更は、他方のリストから見える
+        Vehicle temporary = new Vehicle("test1");
+
+        VehicleLinkedListImpl link1 = new VehicleLinkedListImpl(temporary);
+
+        temporary = new Vehicle("test2");
+        link1.setNextVehicle(new VehicleLinkedListImpl(temporary));
+
+        VehicleLinkedListImpl link2 = new VehicleLinkedListImpl(null);
+        link2 = link1.clone();
+
+        System.out.println(((Vehicle)link1.getVehicle()).getOwner());
+        System.out.println(((Vehicle)link2.getVehicle()).getOwner());
+
+        // link1からもlink2からも変更が見える
+        ((Vehicle)(link2).getVehicle()).setOwner("modified");
+
+        System.out.println("");
+        System.out.println(((Vehicle)link1.getVehicle()).getOwner());
+        System.out.println(((Vehicle)link2.getVehicle()).getOwner());
+
+
+        // １つのリストに対する変更は、他方のリストに影響しない（link2のリストを別の参照にする）
+        temporary = new Vehicle("test3");
+
+        link2 = new VehicleLinkedListImpl(temporary);
+
+        temporary = new Vehicle("test4");
+        link2.setNextVehicle(new VehicleLinkedListImpl(temporary));
+
+        System.out.println("");
+        System.out.println(((Vehicle)link1.getVehicle()).getOwner());
+        System.out.println(((Vehicle)link2.getVehicle()).getOwner());
+
+
+    }
+
+}
