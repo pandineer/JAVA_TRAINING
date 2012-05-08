ad773f9e867b47e0c6506881f69180fe871d7830
Ryosuke Miyahara <r_panda007@yahoo.co.jp>
2012-05-08 20:12:31 +0900

    4.4更新

--- java_training/宮原良介/JPL/ch04/ex04_04/LinkedList.java
+++
@@ -1,9 +0,0 @@
-package ex04_04;
-
-public interface LinkedList {
-	int getObjectNumber();
-	Object getObject();
-	void setObject(Object target);
-	LinkedList getNextObject();
-	void setNextObject(LinkedListImpl target);
-}
