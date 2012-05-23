f38380bfc4f09e3bf371e96eff8dbf757df85b44
Ryosuke Miyahara <r_panda007@yahoo.co.jp>
2012-05-23 20:31:38 +0900

    フォルダ構成変更中

---
+++ java_training/JPL/ch01/ex01_01/HelloWorld.java
@@ -0,0 +1,10 @@
+package ch01.ex01_01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, world");
+
+	}
+
+}
--- java_training/宮原良介/JPL/ch06/ex06_05/Color.java
+++
@@ -1,9 +0,0 @@
-package ex06_05;
-
-public class Color {
-	public String ColorName;
-	Color(String name)
-	{
-		ColorName = name;
-	}
-}
