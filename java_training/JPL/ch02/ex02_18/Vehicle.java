dd8a4dad67534ff966b1dbb8d430a43feba7aded
pandineer <pandineer@gmail.com>
2012-06-04 20:16:28 +0900

    ソースコード見直し中：2章まで完了

--- java_training/JPL/ch02/ex02_18/Vehicle.java
+++ java_training/JPL/ch02/ex02_18/Vehicle.java
@@ -5,135 +5,110 @@
 
 package ch02.ex02_18;
 
-public class Vehicle {
-	private double currentSpeed;
-	private double currentDirection;
-	private String owner;
+public class Vehicle
+{
+    private double currentSpeed;
+    private double currentDirection;
+    private String owner;
 
-	static final int TURN_RIGHT = 1;
-	static final int TURN_LEFT = 2;
+    static final int TURN_RIGHT = 1;
+    static final int TURN_LEFT = 2;
 
-	public void turn(int direction)
-	{
-		if (direction == TURN_RIGHT)
-		{
-			currentDirection = currentDirection + 1.0;
-		}
-		else if (direction == TURN_LEFT)
-		{
-			currentDirection = currentDirection - 1.0;
-		}
-		else
-		{
-			; // 何もしない
-		}
-	}
+    private static int nextID = 1;
+    final int id = nextID++;
 
-	public void turn(double degree)
-	{
-		currentDirection = currentDirection + degree;
-	}
+    public void turn(int direction)
+    {
+        if (direction == TURN_RIGHT)
+        {
+            currentDirection = currentDirection + 1.0;
+        }
+        else if (direction == TURN_LEFT)
+        {
+            currentDirection = currentDirection - 1.0;
+        }
+        else
+        {
+            ; // 何もしない
+        }
+    }
 
-	public String getOwner()
-	{
-		return owner;
-	}
+    public void turn(double degree)
+    {
+        currentDirection = currentDirection + degree;
+    }
 
-	public double getCurrentSpeed()
-	{
-		return currentSpeed;
-	}
+    public String getOwner()
+    {
+        return owner;
+    }
 
-	public void changeSpeed(double speed)
-	{
-		currentSpeed = speed;
-	}
+    public double getCurrentSpeed()
+    {
+        return currentSpeed;
+    }
 
-	public void stop()
-	{
-		currentSpeed = 0.0;
-	}
+    public void changeSpeed(double speed)
+    {
+        currentSpeed = speed;
+    }
 
-	public double getCurrentDirection()
-	{
-		return currentDirection;
-	}
+    public void stop()
+    {
+        currentSpeed = 0.0;
+    }
 
-	public void setCurrentDirection(double direction)
-	{
-		currentDirection = direction;
-	}
+    public double getCurrentDirection()
+    {
+        return currentDirection;
+    }
 
-	private static int nextID = 0;
-	final int id = nextID++;
+    public void setCurrentDirection(double direction)
+    {
+        currentDirection = direction;
+    }
 
-	public Vehicle()
-	{
-		;
-	}
+    public Vehicle()
+    {
+        ;
+    }
 
-	public Vehicle (String ownerName)
-	{
-		owner = ownerName;
-	}
+    public Vehicle(String ownerName)
+    {
+        owner = ownerName;
+    }
 
-	public String toString()
-	{
-		String desc = "owner: " + owner;
+    public String toString()
+    {
+        String desc = "owner: " + owner;
 
-		return desc;
-	}
+        return desc;
+    }
 
-	public static int showCurrentID()
-	{
-		// まだ識別番号が一度も使われていない場合は-1を返す
-		return nextID - 1;
-	}
+    public static int showCurrentID()
+    {
+        // まだ識別番号が一度も使われていない場合は0を返す
+        return nextID - 1;
+    }
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-	    Vehicle testVehicle1;
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        Vehicle testVehicle1;
 
-		if (args.length == 0)
-		{
-			System.out.println("!");
-			testVehicle1 = new Vehicle("Bob");
-		}
-		else
-		{
-			System.out.println("1");
-			testVehicle1 = new Vehicle(args[0]);
-		}
-		testVehicle1.changeSpeed(3.5);
-		testVehicle1.setCurrentDirection(1.2);
-		System.out.println("Id: " + testVehicle1.id);
-		System.out.println("Current speed: " + testVehicle1.getCurrentSpeed());
-		System.out.println("Current direction: " + testVehicle1.getCurrentDirection());
-		System.out.println("Owner: " + testVehicle1.getOwner());
+        if (args.length == 0)
+        {
+            testVehicle1 = new Vehicle("Bob");
+        }
+        else
+        {
+            testVehicle1 = new Vehicle(args[0]);
+        }
 
-		System.out.println("");
+        System.out.println("Owner: " + testVehicle1.getOwner());
 
-		System.out.println("MAX used ID: " + Vehicle.showCurrentID());
-
-		System.out.println("");
-
-		System.out.println(testVehicle1);
-
-		testVehicle1.stop();
-		System.out.println("after stop method, Current speed: " + testVehicle1.getCurrentSpeed());
-
-		System.out.println("current direction: " + testVehicle1.getCurrentDirection());
-		System.out.println("add 0.5 to direction");
-		testVehicle1.turn(0.5);
-		System.out.println("current direction: " + testVehicle1.getCurrentDirection());
-		System.out.println("operate turn right");
-		testVehicle1.turn(TURN_RIGHT);
-		System.out.println("current direction: " + testVehicle1.getCurrentDirection());
-		System.out.println("operate turn left");
-		testVehicle1.turn(TURN_LEFT);
-		System.out.println("current direction: " + testVehicle1.getCurrentDirection());
-	}
+    }
 
 }
