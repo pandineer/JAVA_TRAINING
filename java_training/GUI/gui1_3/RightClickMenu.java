c34e35f2e2045f8128b48d2809f12a2c2bc86387
Ryosuke Miyahara <r_panda007@yahoo.co.jp>
2012-06-13 12:42:38 +0900

    フォントスタイル実装中

--- java_training/GUI/gui1_3/RightClickMenu.java
+++ java_training/GUI/gui1_3/RightClickMenu.java
@@ -36,6 +36,7 @@ import java.awt.event.MouseEvent;
 public class RightClickMenu extends MouseAdapter
 {
     private DigitalClock digitalClock;
+    private MouseEvent startMouse = null;
 
     RightClickMenu(DigitalClock digitalClock)
     {
@@ -44,7 +45,34 @@ public class RightClickMenu extends MouseAdapter
 
     public void mouseReleased(MouseEvent e)
     {
-        if (e.isPopupTrigger())
+        if (1 == e.getButton())
+        {
+            digitalClock.setLocation(digitalClock.getLocation().x + e.getX() - startMouse.getX(),
+                    digitalClock.getLocation().y + e.getY() - startMouse.getY());
+            startMouse = null;
+        }
+        else if (3 == e.getButton())
+        {
+            digitalClock.popup.show(e.getComponent(), e.getX(), e.getY());
+        }
+    }
+
+    public void mouseDragged(MouseEvent e)
+    {
+        if (null != startMouse)
+        {
+        digitalClock.setLocation(digitalClock.getLocation().x + e.getX() - startMouse.getX(),
+                digitalClock.getLocation().y + e.getY() - startMouse.getY());
+        }
+    }
+
+    public void mousePressed(MouseEvent e)
+    {
+        if (1 == e.getButton())
+        {
+            startMouse = e;
+        }
+        else if (3 == e.getButton())
         {
             digitalClock.popup.show(e.getComponent(), e.getX(), e.getY());
         }
