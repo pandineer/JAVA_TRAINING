c6bcc4ffb46503d0d521359b03194f3e96a0b837
pandineer <pandineer@gmail.com>
2012-06-14 12:43:10 +0900

    時刻を保持するオブジェクトをCalendarからDateに変更中

--- java_training/GUI/gui1_3/DigitalClock.java
+++ java_training/GUI/gui1_3/DigitalClock.java
@@ -33,18 +33,19 @@ package gui1_3;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Calendar;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class DigitalClock extends Window implements Runnable, ActionListener
 {
     private static final long serialVersionUID = 1L;
-    private Integer hourInteger; // 時
-    private Integer minuteInteger; // 分
-    private Integer secondInteger; // 秒
 
-    private String hourString;
-    private String minuteString;
-    private String secondString;
+    private Date currentDate;
+    private Date capturedDate;
+    private SimpleDateFormat simpleDataFormat = new SimpleDateFormat("HH:mm:ss");
+
+    private String stringCurrentTime;
+    private String stringCapturedTime;
 
     private Thread th;
     private Image imageBuffer;
@@ -59,9 +60,6 @@ public class DigitalClock extends Window implements Runnable, ActionListener
     private int windowSizeX = 60 * 8 + 50;
     private int windowSizeY = 60 + 50;
 
-    private String timeString = "00:00:00";
-    private String captureTimeString = "00:00:00";
-
     private boolean captureFlag = false;
 
     public PopupMenu popup = new PopupMenu();
@@ -75,27 +73,34 @@ public class DigitalClock extends Window implements Runnable, ActionListener
     private Menu menuFontColor = new Menu("Font Color");
     private Menu menuBackgroundColor = new Menu("BackgroundColor");
 
-    static private String fonts[] = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+    static private String fonts[] = GraphicsEnvironment
+            .getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
 
-    static private String stringFontStyle[] = {"Plain", "Bold", "Italic", "Bold and Italic"};
-    static private int intFontStyle[] = {Font.PLAIN, Font.BOLD, Font.ITALIC, Font.BOLD | Font.ITALIC};
+    static private String stringFontStyle[] =
+    { "Plain", "Bold", "Italic", "Bold and Italic" };
+    static private int intFontStyle[] =
+    { Font.PLAIN, Font.BOLD, Font.ITALIC, Font.BOLD | Font.ITALIC };
 
-    static private String stringColor[] = {"Black", "White", "Red", "Green", "Blue", "Cyan", "Magenta", "Yellow", "Orange"};
-    static private Color colorColor[] = {Color.black, Color.white, Color.red, Color.green, Color.blue, Color.cyan, Color.magenta, Color.yellow, Color.orange};
+    static private String stringColor[] =
+    { "Black", "White", "Red", "Green", "Blue", "Cyan", "Magenta", "Yellow",
+            "Orange" };
+    static private Color colorColor[] =
+    { Color.black, Color.white, Color.red, Color.green, Color.blue, Color.cyan,
+            Color.magenta, Color.yellow, Color.orange };
 
     private RightClickMenu mouse;
 
     // フォントのデフォルトの設定
     private Font fontSetting = new Font(fontType, fontStyle, fontSize);
 
+    // コンストラクタ
     public DigitalClock()
     {
         super(null);
 
-        // 現在時刻用変数の初期化
-        hourInteger = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
-        minuteInteger = Calendar.getInstance().get(Calendar.MINUTE);
-        secondInteger = Calendar.getInstance().get(Calendar.SECOND);
+        // 時刻用変数の初期化
+        currentDate = new Date();
+        capturedDate = null;
 
         property.add(menuFontType);
         {
@@ -152,44 +157,32 @@ public class DigitalClock extends Window implements Runnable, ActionListener
 
     public void paint(Graphics g)
     {
-        // 時・分・秒が一桁の時、0で二桁目を埋める
-        if (hourInteger < 10)
-        {
-            hourString = "0" + hourInteger;
-        }
-        else
-        {
-            hourString = hourInteger.toString();
-        }
-
-        if (minuteInteger < 10)
-        {
-            minuteString = "0" + minuteInteger;
-        }
-        else
-        {
-            minuteString = minuteInteger.toString();
-        }
-
-        if (secondInteger < 10)
-        {
-            secondString = "0" + secondInteger;
-        }
-        else
-        {
-            secondString = secondInteger.toString();
-        }
-        timeString = hourString + ":" + minuteString + ":" + secondString;
-
         if (null != graphicBuffer)
         {
+            // TODO: 現在時刻とキャプチャ時刻の文字列を先に生成し、nullのときとかに対処する
+            stringCurrentTime = simpleDataFormat.format(currentDate);
+            if (capturedDate == null)
+            {
+                stringCapturedTime = "--:--:--";
+            }
+            else
+            {
+                stringCapturedTime = simpleDataFormat.format(capturedDate);
+            }
+
             // ウィンドウサイズの計算
-            if (graphicBuffer.getFontMetrics().stringWidth(timeString) > graphicBuffer.getFontMetrics().stringWidth(captureTimeString))
+            if (graphicBuffer.getFontMetrics().stringWidth(
+                    stringCurrentTime) > graphicBuffer
+                    .getFontMetrics().stringWidth(
+                            simpleDataFormat.format(capturedDate)))
             {
-                windowSizeX = graphicBuffer.getFontMetrics().stringWidth(timeString);
-            } else
+                windowSizeX = graphicBuffer.getFontMetrics().stringWidth(
+                        stringCurrentTime);
+            }
+            else
             {
-                windowSizeX = graphicBuffer.getFontMetrics().stringWidth(captureTimeString);
+                windowSizeX = graphicBuffer.getFontMetrics().stringWidth(
+                        simpleDataFormat.format(capturedDate));
             }
 
             windowSizeX += getInsets().left;
@@ -214,18 +207,29 @@ public class DigitalClock extends Window implements Runnable, ActionListener
         fontSetting = new Font(fontType, fontStyle, fontSize);
         graphicBuffer.setFont(fontSetting); // フォントの設定
         graphicBuffer.setColor(fontColor); // 文字色の設定
-        graphicBuffer.drawString(timeString, 0, graphicBuffer.getFontMetrics()
-                .getAscent() + getInsets().top - getInsets().bottom);
+
+        graphicBuffer.drawString(simpleDataFormat.format(currentDate), 0,
+                graphicBuffer.getFontMetrics().getAscent() + getInsets().top
+                        - getInsets().bottom);
 
         // キャプチャした時刻の描画
         if (true == captureFlag)
         {
-            captureTimeString = timeString;
+            capturedDate = new Date();
             captureFlag = false;
         }
-        graphicBuffer.drawString(captureTimeString, 0,
-                (graphicBuffer.getFontMetrics().getAscent()) * 2
-                        + getInsets().top - getInsets().bottom);
+        if (capturedDate == null)
+        {
+            graphicBuffer.drawString("--:--:--", 0,
+                    (graphicBuffer.getFontMetrics().getAscent()) * 2
+                            + getInsets().top - getInsets().bottom);
+        }
+        else
+        {
+            graphicBuffer.drawString(simpleDataFormat.format(capturedDate), 0,
+                    (graphicBuffer.getFontMetrics().getAscent()) * 2
+                            + getInsets().top - getInsets().bottom);
+        }
 
         // バッファのコピー
         g.drawImage(imageBuffer, 0, 0, this);
@@ -254,10 +258,7 @@ public class DigitalClock extends Window implements Runnable, ActionListener
     {
         while (true)
         {
-            // 現在時刻の獲得
-            hourInteger = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
-            minuteInteger = Calendar.getInstance().get(Calendar.MINUTE);
-            secondInteger = Calendar.getInstance().get(Calendar.SECOND);
+            currentDate = new Date();
 
             // 再描画
             repaint();
@@ -384,7 +385,6 @@ public class DigitalClock extends Window implements Runnable, ActionListener
             System.out.println("This action is not implemented!");
         }
     }
-
 
     public Color changeStringToColor(String colorString)
     {
