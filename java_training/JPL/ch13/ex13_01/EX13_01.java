ab3b25170438721ef534df9c68a1fdfa829369f9
pandineer <pandineer@gmail.com>
2012-06-18 08:52:41 +0900

    練習問題13.2まで完了

---
+++ java_training/JPL/ch13/ex13_01/EX13_01.java
@@ -0,0 +1,34 @@
+/*
+ * 練習問題13.1 p.267
+ * 文字列中に、指定された文字が出現する回数を数えるメソッドを書きなさい。
+ */
+
+package ch13.ex13_01;
+
+public class EX13_01
+{
+
+    static int countSpecificChar(char ch, String str)
+    {
+        int result = 0;
+        for (int i = 0; i < str.length(); i++)
+        {
+            if (ch == str.charAt(i))
+            {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        System.out.println(countSpecificChar('a', "aaabbbccc"));
+        System.out.println(countSpecificChar('a', "abbabcccadddaeeeeeea"));
+        System.out.println(countSpecificChar('a', "bcdefg"));
+    }
+
+}
