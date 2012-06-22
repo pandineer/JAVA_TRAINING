100f10095f8e1b21f6f4be1156aaaf799874fac6
Ryosuke Miyahara <r_panda007@yahoo.co.jp>
2012-06-22 12:35:24 +0900

    練習問題13.5の器だけ作った

---
+++ java_training/JPL/ch13/ex13_05/SplitStringWithComma.java
@@ -0,0 +1,20 @@
+/*
+ * 練習問題13.5 p.291
+ * 10進数を含む文字列を、右から3桁ごとにカンマで区切られた数に変換するメソッドを書きなさい。
+ * たとえば、文字列"1543729"が与えられたら、そのメソッドは、文字列"1,543,729"を返します。
+ */
+
+package ch13.ex13_05;
+
+public class SplitStringWithComma
+{
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO 自動生成されたメソッド・スタブ
+
+    }
+
+}
