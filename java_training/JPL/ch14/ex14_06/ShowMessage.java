8c1f0326fa4ee9994191e9ac4a9b4384a0466fe0
pandineer <pandineer@gmail.com>
2012-07-09 09:00:51 +0900

    練習問題14.6作成中

---
+++ java_training/JPL/ch14/ex14_06/ShowMessage.java
@@ -0,0 +1,45 @@
+/*
+ * 練習問題14.6 p.311
+ * 15秒間隔でメッセージを表示する別のスレッドを持ち、実行開始からの経過時間を表示するプログラムを作成しなさい。
+ * メッセージ表示スレッドは、時間表示スレッドから1秒経過する毎に通知されるようにしなさい。
+ * 時間表示スレッドを修正することなく、7秒間隔で異なるメッセージを表示する別のスレッドを追加しなさい。
+ */
+
+package ch14.ex14_06;
+
+public class ShowMessage implements Runnable
+{
+    String message;
+    long messageInterval;
+    ShowTime showTimeObj;
+    long showedTime = 0;
+
+    public ShowMessage(String message, long messageInterval, ShowTime obj)
+    {
+        this.message = message;
+        this.messageInterval = messageInterval;
+        this.showTimeObj = obj;
+        showedTime = showTimeObj.currentTime;
+    }
+
+    public void run()
+    {
+        try
+        {
+            showMessage();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+
+    public synchronized void showMessage() throws InterruptedException
+    {
+        while (showTimeObj.currentTime - this.showedTime < messageInterval)
+        {
+            wait();
+        }
+        System.out.println(message);
+    }
+}
