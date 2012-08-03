d246fe711f40569982bef9f3b26026b46a3616b7
Ryosuke Miyahara <r_panda007@yahoo.co.jp>
2012-08-03 09:00:51 +0900

    Interpret作成中

--- java_training/Interpret/interpret/Interpret.java
+++ java_training/Interpret/interpret/Interpret.java
@@ -42,22 +42,28 @@ OKが出ない場合には、受講資格を失うこともありますので、
  * フィールドを修正できるInterpretプログラムを作成しなさい
  */
 
+
+// TODO: Interpret用のGUIと、他一般オブジェクト用にGUIを分けるのが吉なのかなぁ。。。
+
+
 package interpret;
 
 import java.awt.*;
-import java.awt.Frame;
 import java.awt.event.*;
-import java.lang.reflect.Member;
+import java.lang.reflect.*;
 
 public class Interpret extends Frame implements ActionListener
 {
     private static final long serialVersionUID = 1L;
-    private static String[] memberName = new String[1000];
     private Class<?> c;
+    private Constructor<?>[] constructor = new Constructor[100];
+    private Type[] constructorArgument = new Type[100];
 
     private TextArea classNameTextArea = new TextArea();
     private Button checkTheClassButton = new Button("Check the class");
     private Choice choiceConstructor = new Choice();
+    private Button selectTheConstructorButton = new Button("Select the constructor");
+    private Choice choiceConstructorArgs = new Choice();
     private Button createInstanceButton = new Button("Create instance");
     private Label errorLabel = new Label("a");
 
@@ -65,6 +71,20 @@ public class Interpret extends Frame implements ActionListener
     {
         // タイトルバーの設定
         super("hoge");
+
+        commonInitialize();
+    }
+
+    public Interpret(String title)
+    {
+        // タイトルバーの設定
+        super(title);
+
+        commonInitialize();
+    }
+
+    private void commonInitialize()
+    {
 
         // ウィンドウを閉じられるようにする
         addWindowListener(new WindowAdapter()
@@ -76,7 +96,7 @@ public class Interpret extends Frame implements ActionListener
         });
 
         // レイアウトの設定
-        this.setLayout(new GridLayout(5, 2));
+        this.setLayout(new GridLayout(7, 2));
         {
             // クラス名
             this.add(new Label("Input class name: "));
@@ -91,6 +111,14 @@ public class Interpret extends Frame implements ActionListener
             this.add(new Label("Constructor: "));
             choiceConstructor.add("---");
             this.add(choiceConstructor);
+
+            // コンストラクタを選ぶ
+            this.add(new Label("Select constructor"));
+            this.add(selectTheConstructorButton);
+
+            // コンストラクタの引数リスト
+            this.add(new Label("Input construtctor argument"));
+            this.add(choiceConstructorArgs);
 
             // インスタンス生成ボタン
             this.add(new Label("Create Instanace: "));
@@ -103,51 +131,6 @@ public class Interpret extends Frame implements ActionListener
         }
 
         setSize(640, 640);
-        setResizable(false);
-        setVisible(true);
-    }
-
-    public Interpret(String title)
-    {
-        // タイトルバーの設定
-        super(title);
-
-        // ウィンドウを閉じられるようにする
-        addWindowListener(new WindowAdapter()
-        {
-            public void windowClosing(WindowEvent e)
-            {
-                System.exit(0);
-            }
-        });
-
-        // レイアウトの設定
-        this.setLayout(new GridLayout(5, 2));
-        {
-            // クラス名
-            this.add(new Label("Input class name: "));
-            this.add(classNameTextArea);
-
-            // クラスチェック
-            this.add(new Label(""));
-            this.add(checkTheClassButton);
-            checkTheClassButton.addActionListener(this);
-
-            // コンストラクタの選択肢を表示する
-            this.add(new Label("Constructor: "));
-            choiceConstructor.add("---");
-            this.add(choiceConstructor);
-
-            // インスタンス生成ボタン
-            this.add(new Label("Create Instanace: "));
-            this.add(createInstanceButton);
-            createInstanceButton.addActionListener(this);
-
-            // エラーメッセージ表示
-            this.add(errorLabel);
-        }
-
-        setSize(640, 480);
         setResizable(false);
         setVisible(true);
     }
@@ -167,12 +150,23 @@ public class Interpret extends Frame implements ActionListener
         {
             try
             {
-                c.newInstance();
+                // c.newInstance();
+                // c.getConstructors()[1].newInstance("mogemoge");
+                System.out.println(choiceConstructor.getSelectedIndex());
+
+
             }
             catch(Exception ex)
             {
                 System.out.println(ex);
             }
+        }
+
+        // Select the constructorボタン
+        if ("Select the constructor" == e.getActionCommand())
+        {
+            // TODO: getGenericParameterTypes()を使って、コンストラクタの引数の型一覧を取得・表示する
+            for(int i = 0; i < )
         }
     }
 
@@ -186,42 +180,38 @@ public class Interpret extends Frame implements ActionListener
         }
         catch (ClassNotFoundException e)
         {
-            // System.out.println("unknown class: " + className);
             errorLabel.setText("Unknown class: " + className);
         }
     }
 
 
-    private void addConstructor(Member[] mems, boolean isFirst)
+    private void addConstructor(Constructor<?>[] tempConstructor, boolean isFirst)
     {
-        Member m;
-
         if (true == isFirst)
         {
             choiceConstructor.removeAll();
         }
-        checkMember: for (int i = 0; i < mems.length; i++)
+        checkMember: for (int i = 0; i < tempConstructor.length; i++)
         {
-            m = mems[i];
-            if (m.getDeclaringClass() == Object.class)
+            if (tempConstructor[i].getDeclaringClass() == Object.class)
             {
                 continue;
             }
             if (true == isFirst)
             {
-                memberName[i] = m.toString();
+                constructor[i] = tempConstructor[i];
             }
             if (false == isFirst)
             {
-                for (int j = 0; j < memberName.length; j++)
+                for (int j = 0; j < constructor.length; j++)
                 {
-                    if (m.toString().equals(memberName[j]))
+                    if (tempConstructor[i].toString().equals(constructor[j].toString()))
                     {
                         continue checkMember;
                     }
                 }
             }
-            choiceConstructor.add(m.toString());
+            choiceConstructor.add(tempConstructor[i].toString());
         }
     }
 
