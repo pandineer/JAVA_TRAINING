1fca2c4535b7825cf19a3d83d0eb1e09bafe0149
Ryosuke Miyahara <r_panda007@yahoo.co.jp>
2012-10-16 22:59:22 +0900

    練習問題23-1作成中

--- JAVATRAINING_ROOT/JPL/ch23/ex23_01/Plug.java
+++ JAVATRAINING_ROOT/JPL/ch23/ex23_01/Plug.java
@@ -5,9 +5,12 @@
 
 package ch23.ex23_01;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintStream;
 
 public class Plug
 {
@@ -27,20 +30,46 @@ public class Plug
     // 2つのストリームを結合するためのメソッド
     public static void plugTogether(InputStream in, OutputStream out)
     {
-
+        int b;
+        // InputStreamReader ir = new InputStreamReader(in);
+        try
+        {
+            while ((b = in.read()) != -1)
+            // for (int i = 0; i < 5; i++)
+            {
+                out.write(b);
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
     }
 
     // 2つのストリームを結合するためのメソッド
-    public static void plugTogether(OutputStream out, InputStream in)
+    public static void plugTogether(PrintStream out, InputStream in)
     {
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String line;
+        try
+        {
+            while((line = br.readLine()) != null)
+            {
+                out.println(line);
+            }
+        }
+        catch(Exception e)
+        {
+            out.println(e);
+        }
     }
 
     public static void main(String[] args)
     {
         try
         {
-            Plug.userProg("notepad.exe");
+            // Plug.userProg("ping -n 3 127.0.0.1");
+            Plug.userProg("sort.exe");
         }
         catch(Exception e)
         {
