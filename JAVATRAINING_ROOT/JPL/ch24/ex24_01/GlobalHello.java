7830c61ada4e3a6efe84238afa764988d20f6440
Ryosuke Miyahara <r_panda007@yahoo.co.jp>
2012-10-23 23:24:23 +0900

    練習問題24.1完了

--- JAVATRAINING_ROOT/JPL/ch24/ex24_01/GlobalHello.java
+++ JAVATRAINING_ROOT/JPL/ch24/ex24_01/GlobalHello.java
@@ -12,11 +12,15 @@ public class GlobalHello
 {
     public static void main(String[] args)
     {
-        ResourceBundle res = ResourceBundle.getBundle("GlobalRes");
+        // Locale.setDefault(new Locale("en", "AU"));
+        // Locale.setDefault(Locale.CHINA);
+        // Locale.setDefault(new Locale("ho", "GE"));
+        Locale.setDefault(new Locale("te", "XT"));
+        ResourceBundle res = ResourceBundle.getBundle("ch24.ex24_01.GlobalRes");
         String msg;
         if (args.length > 0)
         {
-            msg = res.getString(GlobalRes.GoodBYE);
+            msg = res.getString(GlobalRes.GOODBYE);
         }
         else
         {
