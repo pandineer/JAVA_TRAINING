0daa68776114af8f8fe419975d77e3d334d9262d
pandineer <pandineer@gmail.com>
2012-10-30 13:02:21 +0900

    練習問題17.2修正中

--- JAVATRAINING_ROOT/JPL/ch17/ex17_02/DataHandler.java
+++ JAVATRAINING_ROOT/JPL/ch17/ex17_02/DataHandler.java
@@ -14,6 +14,8 @@ public class DataHandler
     private WeakReference<File> lastFile;
     private WeakReference<byte[]> lastData; // （おそらく）最後のデータ
 
+    Runtime rt = Runtime.getRuntime();
+
     byte[] readFile(File file)
     {
         byte[] data;
@@ -29,23 +31,57 @@ public class DataHandler
         }
 
         // 記憶していないので、読み込む
-        data = readBytesFromFile(file);
+        // data = readBytesFromFile(file);
         // lastFile = file; // Original
         lastFile = new WeakReference<File>(file);
-        lastData = new WeakReference<byte []>(data);
-        return data;
+        // lastData = new WeakReference<byte []>(data);
+        // eturn data;
+        return null;
     }
 
     public byte[] readBytesFromFile(File file)
     {
         // 中身は適当
         file.toString();
-        return new byte[5];
+        return new byte[80000000];
+    }
+
+    public void checkLastFile()
+    {
+        if (lastData != null)
+        {
+            System.out.println("lastFile has data. ");
+            System.out.println(lastFile);
+        }
+        else
+        {
+            System.out.println("lastFile does not have data. ");
+        }
+    }
+
+    public void garbateCollect()
+    {
+        rt.gc();
+    }
+
+    public void showFreeMemory()
+    {
+        System.out.println("Free memory is: " + rt.freeMemory());
     }
 
     public static void main(String[] args)
     {
-        // テスト…？
+        DataHandler test = new DataHandler();
+        test.showFreeMemory();
+        test.checkLastFile();
+        test.readFile(new File("test"));
+        test.showFreeMemory();
+        test.checkLastFile();
+        test.garbateCollect();
+        test.showFreeMemory();
+        int[] tmp = new int[40000000];
+        test.checkLastFile();
+
     }
 
 }
