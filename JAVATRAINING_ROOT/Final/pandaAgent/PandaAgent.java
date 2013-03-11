8f559074dd46d6a8f11ac8a173b49267ccaa5b01
pandineer <pandineer@gmail.com>
2013-03-11 21:08:50 +0900

    余計なコード削除

--- JAVATRAINING_ROOT/Final/pandaAgent/PandaAgent.java
+++ JAVATRAINING_ROOT/Final/pandaAgent/PandaAgent.java
@@ -21,14 +21,10 @@ public class PandaAgent extends JFrame
     private final int pandaSizeX = 240;
     private final int pandaSizeY = 420;
 
-    // private final BoxLayout boxlayout = new BoxLayout(this.getContentPane(), BoxLayout.X_AXIS);
-    // private final GridBagLayout layout = new GridBagLayout();
     private final FlowLayout layout = new FlowLayout();
-    private GridBagConstraints gbc = new GridBagConstraints();
 
     // Component
     private final ImagePanel balloonPanel = new ImagePanel("/pandaAgent/balloon.png");
-    // public JPanel balloonPanel = new JPanel();
     private final ImagePanel pandaPanel = new ImagePanel("/pandaAgent/panda_agent_close.png");
 
     public PandaAgent()
@@ -52,35 +48,11 @@ public class PandaAgent extends JFrame
         this.setResizable(false);
         this.setVisible(true);
 
-        gbc.fill = GridBagConstraints.BOTH;
-
         // Create Panel
-        balloonPanel.setSize((windowSizeX - pandaSizeX), windowSizeY);
         balloonPanel.setPreferredSize(new Dimension(380, 240));
-        // balloonPanel.setBackground(Color.red);
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        // gbc.gridwidth = 3;
-        // gbc.gridheight = 1;
-        // gbc.anchor = GridBagConstraints.WEST;
-
-        // layout.setConstraints(balloonPanel, gbc);
-
-        // balloonPanel.setSize(100, windowSizeY);
         this.add(balloonPanel);
 
-        // pandaPanel.setSize(pandaSizeX, pandaSizeY);
-        // pandaPanel.setSize(pandaSizeX, pandaSizeY);
         pandaPanel.setPreferredSize(new Dimension(pandaSizeX, pandaSizeY));
-        gbc.gridx = 3;
-        gbc.gridy = 0;
-        // gbc.gridwidth = 1;
-        // gbc.gridheight = 1;
-        // gbc.anchor = GridBagConstraints.EAST;
-
-        // layout.setConstraints(pandaPanel, gbc);
-        // pandaPanel.setBounds(400, 0, 240, 420);
-
         this.add(pandaPanel);
 
         balloonPanel.add(new JLabel("Test"));
