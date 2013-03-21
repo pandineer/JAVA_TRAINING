ae0faca21bfca161924576a7513f5e4f6d1c0b9e
Ryosuke Miyahara <p000507390@ZRFG027986.rfg.rfgad.ricoh.com>
2013-03-21 12:26:08 +0900

    ウインドウをドラッグで移動できるようにした

---
+++ JAVATRAINING_ROOT/Final/pandaAgent/Mouse.java
@@ -0,0 +1,53 @@
+package pandaAgent;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Mouse extends MouseAdapter
+{
+    private PandaAgent pandaAgent;
+    private MouseEvent startMouse = null;
+
+    public Mouse(PandaAgent pandaAgent)
+    {
+        this.pandaAgent = pandaAgent;
+    }
+
+    public void mouseReleased(MouseEvent e)
+    {
+        if (1 == e.getButton())
+        {
+            pandaAgent.setLocation(pandaAgent.getLocation().x + e.getX() - startMouse.getX(),
+                    pandaAgent.getLocation().y + e.getY() - startMouse.getY());
+            startMouse = null;
+        }
+        else if (3 == e.getButton())
+        {
+            ;
+        }
+    }
+
+    public void mouseDragged(MouseEvent e)
+    {
+        if (null != startMouse)
+        {
+            pandaAgent.setLocation(pandaAgent.getLocation().x + e.getX() - startMouse.getX(),
+                    pandaAgent.getLocation().y + e.getY() - startMouse.getY());
+        }
+    }
+
+    public void mousePressed(MouseEvent e)
+    {
+        if (1 == e.getButton())
+        {
+            startMouse = e;
+        }
+        else if (3 == e.getButton())
+        {
+            if (e.isPopupTrigger())
+            {
+                ;
+            }
+        }
+    }
+}
